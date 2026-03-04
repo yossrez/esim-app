3cdcd98cdf9bbad28f907038974626f69ac451cc
yossrez <yosssdev@gmail.com>
2026-03-04 16:56:11 +0700

    setup next@14.x

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import localFont from "next/font/local";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function Home() {
+  return (
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-(family-name:--font-geist-sans)`}
+    >
+      asdf
+    </div>
+  );
+}
